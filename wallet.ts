1bff65f80d2beb95d67cef6f1124a7233caf39ad
Steve Kok <kokchoquan@gmail.com>
2025-12-05 23:40:08 +0800

    feat: edward account 6

--- wallet.ts
+++ wallet.ts
@@ -7,39 +7,19 @@ export const dustManagerWallet = {
 };
 
 export const dustBorrowWallets = [
+    // {
+    //     name: 'Dust Borrower 5',
+    //     wallet: new ethers.Wallet(process.env.DUST_KEY_5!, jsonRpcProvider),
+    // },
     {
-        name: 'Dust Borrower 5',
-        wallet: new ethers.Wallet(process.env.DUST_KEY_5!, jsonRpcProvider),
+        name: 'Edward Account 6',
+        wallet: new ethers.Wallet(process.env.EDWARD_KEY_6!, jsonRpcProvider),
     },
 ];
 
 export const junkWallets = [
     {
-        name: 'Edward Account 1',
-        wallet: new ethers.Wallet(process.env.EDWARD_KEY_1!, jsonRpcProvider),
-    },
-    {
         name: 'Edward Account 3',
         wallet: new ethers.Wallet(process.env.EDWARD_KEY_3!, jsonRpcProvider),
-    },
-    {
-        name: 'Edward Account 4',
-        wallet: new ethers.Wallet(process.env.EDWARD_KEY_4!, jsonRpcProvider),
-    },
-    {
-        name: 'Steve Account 1',
-        wallet: new ethers.Wallet(process.env.STEVE_KEY_1!, jsonRpcProvider),
-    },
-    {
-        name: 'Aescobar Account 1',
-        wallet: new ethers.Wallet(process.env.AESCOBAR_KEY_1!, jsonRpcProvider),
-    },
-    {
-        name: 'Dust Borrower 3',
-        wallet: new ethers.Wallet(process.env.DUST_KEY_3!, jsonRpcProvider),
-    },
-    {
-        name: 'Dust Borrower 4',
-        wallet: new ethers.Wallet(process.env.DUST_KEY_4!, jsonRpcProvider),
     },
 ];
