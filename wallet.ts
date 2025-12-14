18e35d1867757d7902e9740c02c73ea273e8a41a
Steve Kok <kokchoquan@gmail.com>
2025-12-14 16:21:55 +0800

    feat: dust borrower 7

--- wallet.ts
+++ wallet.ts
@@ -6,13 +6,14 @@ export const dustManagerWallet = {
     wallet: new ethers.Wallet(process.env.DUST_MANAGER!, jsonRpcProvider),
 };
 
-export const dustBorrowWallets = [];
+export const dustBorrowWallets = [
+    {
+        name: 'Dust Borrower 7',
+        wallet: new ethers.Wallet(process.env.DUST_KEY_7!, jsonRpcProvider),
+    },
+];
 
 export const junkWallets = [
-    {
-        name: 'Dust Borrower 5',
-        wallet: new ethers.Wallet(process.env.DUST_KEY_5!, jsonRpcProvider),
-    },
     {
         name: 'Dust Borrower 6',
         wallet: new ethers.Wallet(process.env.DUST_KEY_6!, jsonRpcProvider),
